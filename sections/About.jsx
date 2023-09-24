9189d9c5235ea0ef3b1172b41831706688e4cada
RubenAcosta16 <1511ruben@gmail.com>
2023-09-23 20:17:42 -0600

    first commit

---
+++ sections/About.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10 `}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText
+        title="|About Metaversus"
+        textStyles="text-center"
+      ></TypingText>
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Lorem</span>
+        ipsum dolor sit amet consectetur adipisicing{" "}
+        <span className="font-extrabold text-white">elit.</span> Maxime,
+        asperiores voluptas? Commodi, laboriosam!{" "}
+        <span className="font-extrabold text-white">Maiores modi</span> aliquid,
+        soluta esse nostrum corporis rem, fuga minus dolore sed eaque, ducimus
+        reiciendis ratione facilis?5
+      </motion.p>
+
+      <motion.img
+        src="/arrow-down.svg"
+        alt="arrow down"
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="w-[148px] h-[28px] object-contain mt-[20px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
---
+++ sections/World.jsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion";
+import { TitleText, TypingText } from "../components";
+
+import { exploreWorlds } from "../constants";
+
+const World = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex  flex-col`}
+    >
+      <TypingText title="| People  on the world" textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Track your friends around you and invite them to play together in
+            the same world
+          </>
+        }
+        textStyles="text-center"
+      />
+
+      {/* people divs */}
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-01.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-02.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-03.png" alt="people" className="w-full h-full" />
+        </div>
+
+        {/* pictures divs */}
+
+        <div className="absolute top-[40%] left-[18%] max-w-[250px] h-[170px] p-[10px] bg-[#5d6680] rounded-[24px]">
+          <img
+            src={exploreWorlds[0].imgUrl}
+            alt="people"
+            className="w-full h-full object-cover rounded-[24px]"
+          />
+
+          <p className="text-white absolute bottom-0 left-1/4 text-[22px]">
+            {exploreWorlds[0].title}
+          </p>
+        </div>
+
+        <div className="absolute top-[20%] left-[70%] max-w-[250px] h-[170px] p-[10px] bg-[#5d6680] rounded-[24px]">
+          <img
+            src={exploreWorlds[1].imgUrl}
+            alt="people"
+            className="w-full h-full object-cover rounded-[24px]"
+          />
+
+          <p className="text-white absolute bottom-0 left-1/4 text-[22px]">
+            {exploreWorlds[1].title}
+          </p>
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default World;
